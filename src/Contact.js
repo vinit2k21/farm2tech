6840915b975982124918ace7616fdef90a1fc1a4
vinit2k21 <en20169879@git-india.edu.in>
2023-08-20 21:04:22 +0530

    Add Contact Page

--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
   return (
-    <div>Farm2Tech aims to harness digitalization and technological advancements in agriculture to foster rural development. The review aims to understand the significance of digital technologies in management of agricultural products towards end user, identify the challenges and opportunities associated with implementing such projects, and examine existing studies on the impacts of digitalization in rural communities. By implementing these digitalisation solutions, Farm2Tech aims to bridge the technological gap for rural farmers, improving their access to information, markets, and modern agricultural practices. Through this integrated approach, Farm2Tech will support rural development in various sector, empowering farmers and fostering sustainable growth in rural communities. Additionally, the project will work to provide training and support to help farmers and consumers adopt and adapt to digital technologies effectively.</div>
+    <div className='contact-container'>
+      <div className='contact-info'>
+        <h2 className='contact-info-h2'>Our Office Address</h2>
+        <p className='contact-info-p'>Farm2Tech,Khed,Ratnagiri,415605</p>
+        <h2 className='contact-info-h2'>General Enquiries</h2>
+        <p className='contact-info-p'>farm2tech@git-india.edu.in</p>
+        <h2 className='contact-info-h2'>Call Us</h2>
+        <p className='contact-info-p'>+91-0000000000</p>
+        <h2 className='contact-info-h2'>Our Timing</h2>
+        <p className='contact-info-p'>Mon - Sun : 09:00 AM - 09:00 PM</p>
+      </div>
+      <div className='contact-form'>
+        <form>
+          <h1 className='contact-form-heading'>Contact Form</h1>
+          <input className='contact-form-inputs' type="text" placeholder='Full Name' />
+          <input className='contact-form-inputs' type="tel" placeholder='Mobile Number'  pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" maxlength="10" />
+          <input className='contact-form-inputs' type="email" placeholder='Email ID' />
+          <textarea className='contact-form-inputs' type="text" rows="10" cols="50" placeholder='Message'></textarea>
+          <button className='contact-form-submit-button' type="submit" value="Submit" >Submit</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
